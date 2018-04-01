e3213fde0db7ee1cf1b30bb2aeee8529fce5c4c2
fuldaros <litefros@gmail.com>
2018-04-02 02:30:24 +0300

    use AKB 0.6

--- build.sh
+++ build.sh
@@ -10,40 +10,51 @@ $cy*                   by fuldaros                    *
 $cy****************************************************
 $r!!! Пожалуйста, уважайте чужой труд и не меняйте имя автора на свое <3 !!! $y";     
 set -e
-author=$(sed -n 2p make.prop);
+usr=$(sed -n 2p make.prop);
 bh=$(sed -n 8p make.prop);
 arch=$(sed -n 4p make.prop);
-stamp=$(date +"%d.%m.%Y %H:%M");
+stamp=$(date +"%Y.%m.%d %H:%M");
 stampt=$(date +"%d.%m.%Y-%H:%M");
-kernel=paperplane_"$stamp";
 logb=logb_"$stamp";
-otazip=ota_pp_"$stamp";
-device=$(sed -n 14p make.prop);
+otazip=ota_akb_"$stamp";
+device=$(sed -n 12p make.prop);
 cpu=$(sed -n 10p make.prop);
-cpum=$(sed -n 12p make.prop);
+imgt=$(sed -n 14p make.prop);
+loc=$(sed -n 18p make.prop);
+gcc=$(sed -n 16p make.prop);
+sha="0"
+if [[ "$sha" != "1" ]]
+then
+type="USER";
+else
+type="OFFICIAL";
+fi
+kernel="$imgt"_akb_"$stamp";
 export ARCH="$arch"
 export TARGET_ARCH="$arch"
 export KBUILD_BUILD_USER="$author"
 export KBUILD_BUILD_HOST="$bh"
 echo -e "$cy******************************$y"
 echo -e "$g   Build info";
-echo -e "$y User: "$author"
+echo -e "$y User: "$usr"
  Host: "$bh"
  ARCH: "$arch"
- CPU: "$cpum" "$cpu"
+ CPU: "$cpu"
  Device: "$device"
- Build time: "$stamp"";
+ Build time: "$stamp"
+ Kernel location: "$loc"
+ Build type: "$type"";
 echo -e "$cy******************************$y"
-rm -rf out/paperplane/include/generated/compile.h
+rm -rf out/akb_"$device"/include/generated/compile.h
 pwd > pwd.dat
 read pwd < pwd.dat
 rm -f pwd.dat
-export CROSS_COMPILE="$pwd"/tools/bin/aarch64-linux-gnu-
+export CROSS_COMPILE="$pwd"/tools/"$gcc"
 cd sources/
 echo -e "$g Внимание, подождите. Наводим Тополь-M на Соедененные Штаты Америки.
  Терпения, друзья! :3$y"
 strt=$(date +"%s")
-make -j3 O=../out/paperplane Image.gz-dtb > ../outkernel/"$logb"
+make -j3 O=../out/akb_"$device" "$imgt" > ../outkernel/"$logb"
 clear
 echo -e "
 $cy****************************************************
@@ -52,8 +63,8 @@ $cy*                   by fuldaros                    *
 $cy****************************************************
 $y";  
 echo -e "$g Идет обратый отсчет.$y"
-cat ../out/paperplane/arch/arm64/boot/Image.gz-dtb > ../outkernel/"$kernel"
-rm -rf ../out/paperplane/arch/arm64/boot/
+cat ../out/akb_"$device"/arch/"$arch"/boot/"$imgt" > ../outkernel/"$kernel"
+rm -rf ../out/akb_"$device"/arch/"$arch"/boot/
 cd ../
 echo -e "$g Пуск. Тополь-М приближается к цели...$y"
 rm -f otagen/zImage
@@ -64,6 +75,7 @@ cat ../make.prop > author.prop
 echo -e "//BUILD TIME" "\n""$stampt" >> author.prop
 echo -e "// Automatic kernel builder ver. (DONT EDIT)" >> author.prop
 echo -e "$ver" >> author.prop
+echo -e "//BUILD TYPE" "\n""$type" >> author.prop
 zip -r ../outzip/"$otazip".zip *
 echo -e "$g Поздравляем, Соедененные Штаты Америки стерты с лица земли.
  Подробности в этом архиве. "$otazip".zip$y"
@@ -74,3 +86,4 @@ end=$(date +"%s")
 diff=$(( $end - $strt ))
 echo Операция выполнена успешно!
 echo -e "$m Полет Тополь-M до цели занял "$diff" секунд!"
+####### script v08 (beta)
--- otagen/flashme.sh
+++ otagen/flashme.sh
@@ -2,19 +2,13 @@
 ## OTA setup
 # begin properties
 properties() {
-kernel.string=1.5
 do.devicecheck=1
-do.modules=0
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=porridge
-device.name2=spark
-device.name3=alps6735
-device.name4=
-device.name5=
 } # end properties
 # shell variables
-block=/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/boot
+loc=$(sed -n 18p /tmp/otapack/author.prop);
+block="$loc"
 is_slot_device=0;
 ## OTA methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
